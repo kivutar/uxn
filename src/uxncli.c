acf6dcee489be583a7cab0e35671da32a1cc5619
neauoire <aliceffekt@gmail.com>
2021-08-16 18:45:27 -0700

    Removed unused macro in console example

--- src/uxncli.c
+++ src/uxncli.c
@@ -133,7 +133,7 @@ static void
 run(Uxn *u)
 {
 	uxn_eval(u, PAGE_PROGRAM);
-	while(read(0, &devconsole->dat[0x2], 1) > 0) 
+	while(read(0, &devconsole->dat[0x2], 1) > 0)
 		uxn_eval(u, mempeek16(devconsole->dat, 0));
 }
 
